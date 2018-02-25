b340133dd13cb499dd7e3015c419e1c526611dcf
Eric Newton <ericnewton76@gmail.com>
2018-02-25 12:39:30 -0500

    slight mod to status to create (better) prototype implementation and add comments

--- bin/cli_util.js
+++ bin/cli_util.js
@@ -9,56 +9,67 @@ function createLogger() {
   return log;
 }
 
-statusImpl = function(){
+statusImpl = function() {
 
-  var verbose = false;
   var chalkInfo = chalk.reset; //basically use default
   var chalkError = chalk.red;
   var chalkWarn = chalk.yellow;
 
-  statusImpl.prototype.banner = function(stream) {
+}
+
+statusImpl.prototype.options = {
+  verbose: false,
+  quiet: false
+};
+
+/// writes banner to stdout when quiet==false
+statusImpl.prototype.banner = function(stream) {
+  if(this.options.quiet === false) {
     stream.write('\n');
     stream.write('CrownPeak AccessAPI CLI\n');
     stream.write('\n');
   }
-
-  statusImpl.prototype.info = function(text) {
-    var argsArray = Array.prototype.slice.call(arguments);
-
-    if(log !== undefined && log.isInfoEnabled !== false) {
-      log.info.apply(log, argsArray);
-    }
-
-    var str = util.format.apply(null,argsArray);
-    process.stdout.write(chalkInfo(str));
-    process.stdout.write('\n');
-  }
-
-  statusImpl.prototype.warn = function(text) {
-    var argsArray = Array.prototype.slice.call(arguments);
-
-    if(log !== undefined && log.isWarnEnabled !== false) {
-      log.warn.apply(log, argsArray);
-    }
-
-    var str = util.format.apply(null,argsArray);
-    process.stderr.write(chalkWarn(str));
-    process.stderr.write('\n');
-  }
-
-  statusImpl.prototype.error = function(text) {
-    var argsArray = Array.prototype.slice.call(arguments);
-
-    if(log !== undefined && log.isErrorEnabled !== false) {
-      log.fatal.apply(log, argsArray);
-    }
-
-    var str = util.format.apply(null,argsArray);
-    process.stderr.write(chalkError(str));
-    process.stderr.write('\n');
-  }
-
 }
+
+/// writes info message to stdout and to logs
+statusImpl.prototype.info = function(text) {
+  var argsArray = Array.prototype.slice.call(arguments);
+
+  if(log !== undefined && log.isInfoEnabled !== false) {
+    log.info.apply(log, argsArray);
+  }
+
+  var str = util.format.apply(null,argsArray);
+  process.stdout.write(chalkInfo(str));
+  process.stdout.write('\n');
+}
+
+/// writes warning message to stdout and to logs
+statusImpl.prototype.warn = function(text) {
+  var argsArray = Array.prototype.slice.call(arguments);
+
+  if(log !== undefined && log.isWarnEnabled !== false) {
+    log.warn.apply(log, argsArray);
+  }
+
+  var str = util.format.apply(null,argsArray);
+  process.stderr.write(chalkWarn(str));
+  process.stderr.write('\n');
+}
+
+/// writes error message to stderr and to logs
+statusImpl.prototype.error = function(text) {
+  var argsArray = Array.prototype.slice.call(arguments);
+
+  if(log !== undefined && log.isErrorEnabled !== false) {
+    log.fatal.apply(log, argsArray);
+  }
+
+  var str = util.format.apply(null,argsArray);
+  process.stderr.write(chalkError(str));
+  process.stderr.write('\n');
+}
+
 var statusSingleton = new statusImpl();
 
 asListFunction = function(val) {
