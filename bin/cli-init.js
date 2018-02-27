a78dc8e74bed029d63681eb45a25fa99633e2c45
Eric Newton <ericnewton76@gmail.com>
2018-02-27 17:46:50 -0500

    some refactoring to eventually support testing

--- bin/cli-init.js
+++ bin/cli-init.js
@@ -12,114 +12,8 @@ program
   .name('init')
   .parse(process.argv)
 
-console.log('- Initialize %s', constants.configJsonName);
-console.log();
-console.log(chalk.yellow('Note that the CrownPeak AccessAPI requires the use of an API Key.'));
-console.log('Contact CrownPeak support at support@crownpeak.com to request a key.');
-console.log();
-console.log();
 
-var currentValues = {};
-if(fs.existsSync('accessapi-config.json')) {
-  
-  try {
-    currentValues = JSON.parse(fs.readFileSync(constants.configJsonName, { encoding:"utf8" }));
-  } 
-  catch(e) {
-    console.log(chalk.red('Warning: Failed to read current %s',constants.configJsonName));
-    console.log();
-  }
-}
-
-//https://github.com/flatiron/prompt#valid-property-settings
-var properties = [
-  { 
-    name: 'instance'
-    ,default: currentValues["instance"]
-    ,message: 'Name of the CrownPeak instance'
-    ,pattern: /^\w+$/
-  }
-  ,{
-    name: 'domain'
-    ,default: currentValues["domain"] || "cms.crownpeak.net"
-    ,message: 'Domain of api service'
-  }
-  ,{
-    name: 'apikey'
-    ,message: 'The API key to access the instance with'
-    ,default: currentValues.apikey
-  }
-  ,{
-    name: 'username'
-    ,message: 'Username to use to access. Leave blank for OS password chain support.'
-    ,default: currentValues.username
-  }
-  ,{
-    name: 'password'
-    ,hidden: true
-    ,replace: '*'
-    ,message: 'Password to use to access (stored in OS keychain)'
-    //,ask: function() {
-    //  ////only ask if the username was entered.
-    //  //return prompt.history('username').value > 0;
-    //}
-  }
-];
-
-
-prompt.message = "Enter ";
-prompt.delimiter = chalk.green(':');
-prompt.start();
-
-prompt.get(properties, function(err,result) {
-  if(err) { return onErr(err); }
-  
-  result["cms_instance_url"] = "https://" + result["domain"] + "/" + result["instance"];
-  
-  return new Promise((resolve,reject) => {
-    var keytar;
-    try { keytar = require('keytar'); }
-    catch(ex) { throw 'Failed to load keytar.'; }
-
-    var service = 'UN=' + result.username + ';CN=' + result.instance;
-    keytar.setPassword('Crownpeak-AccessAPI-NodeJS', service, result.password).then(()=>{
-      console.log(chalk.green('Stored password into OS keychain.'));
-      resolve();
-    })
-  
-  }).then(()=> {
-
-    var showOptions = Object.assign({}, result);
-    delete showOptions.password;
-
-    console.log('Here is the options:');
-    console.log(JSON.stringify(showOptions,null,2));
-    console.log();
-
-    checkConfig(result)
-    .then(()=>{
-
-      prompt.get([{name:'write_file',message:'Write to file?',validator: /[y|n]/}], (err,questionResult)=> {
-
-        if(questionResult["write_file"] === 'y') {
-          delete result.password;
-
-          //write the options into accessapi-config.json file
-          fs.writeFileSync('./' + constants.configJsonName, JSON.stringify(result,null,2), 'utf-8');
-    
-          console.log();
-          console.log('Wrote answers to %s', constants.configJsonName);
-        }
-
-      });
-
-    })
-
-  })
-
-});
-
-function checkConfig(options) {
+let askTestConfigOptions = function (options) {
 
   return new Promise((resolve,reject)=> {
 
@@ -154,7 +48,116 @@ function checkConfig(options) {
 
 }
 
+let main = function (program) {
 
+  console.log('- Initialize %s', constants.configJsonName);
+  console.log();
+  console.log(chalk.yellow('Note that the CrownPeak AccessAPI requires the use of an API Key.'));
+  console.log('Contact CrownPeak support at support@crownpeak.com to request a key.');
+  console.log();
+  console.log();
+  
+  var currentValues = {};
+  if(fs.existsSync('accessapi-config.json')) {
+    
+    try {
+      currentValues = JSON.parse(fs.readFileSync(constants.configJsonName, { encoding:"utf8" }));
+    } 
+    catch(e) {
+      console.log(chalk.red('Warning: Failed to read current %s',constants.configJsonName));
+      console.log();
+    }
+  }
+  
+  //https://github.com/flatiron/prompt#valid-property-settings
+  var properties = [
+    { 
+      name: 'instance'
+      ,default: currentValues["instance"]
+      ,message: 'Name of the CrownPeak instance'
+      ,pattern: /^\w+$/
+    }
+    ,{
+      name: 'domain'
+      ,default: currentValues["domain"] || "cms.crownpeak.net"
+      ,message: 'Domain of api service'
+    }
+    ,{
+      name: 'apikey'
+      ,message: 'The API key to access the instance with'
+      ,default: currentValues.apikey
+    }
+    ,{
+      name: 'username'
+      ,message: 'Username to use to access. Leave blank for OS password chain support.'
+      ,default: currentValues.username
+    }
+    ,{
+      name: 'password'
+      ,hidden: true
+      ,replace: '*'
+      ,message: 'Password to use to access (stored in OS keychain)'
+      //,ask: function() {
+      //  ////only ask if the username was entered.
+      //  //return prompt.history('username').value > 0;
+      //}
+    }
+  ];
+  
+  
+  prompt.message = "Enter ";
+  prompt.delimiter = chalk.green(':');
+  prompt.start();
+  
+  prompt.get(properties, function(err,result) {
+    if(err) { return onErr(err); }
+    
+    result["cms_instance_url"] = "https://" + result["domain"] + "/" + result["instance"];
+    
+    return new Promise((resolve,reject) => {
+      var keytar;
+      try { keytar = require('keytar'); }
+      catch(ex) { throw 'Failed to load keytar.'; }
+  
+      var service = 'UN=' + result.username + ';CN=' + result.instance;
+      keytar.setPassword('Crownpeak-AccessAPI-NodeJS', service, result.password).then(()=>{
+        console.log(chalk.green('Stored password into OS keychain.'));
+        resolve();
+      })
+    
+    }).then(()=> {
+  
+      var showOptions = Object.assign({}, result);
+      delete showOptions.password;
+  
+      console.log('Here is the options:');
+      console.log(JSON.stringify(showOptions,null,2));
+      console.log();
+  
+      askTestConfigOptions(result)
+      .then(()=>{
+  
+        prompt.get([{name:'write_file',message:'Write to file?',validator: /[y|n]/}], (err,questionResult)=> {
+  
+          if(questionResult["write_file"] === 'y') {
+            delete result.password;
+  
+            //write the options into accessapi-config.json file
+            fs.writeFileSync('./' + constants.configJsonName, JSON.stringify(result,null,2), 'utf-8');
+      
+            console.log();
+            console.log('Wrote answers to %s', constants.configJsonName);
+          }
+  
+        });
+  
+      })
+  
+    })
+  
+  });
+  
+}(program);
 
 function onErr(err) {
   console.log(err);
